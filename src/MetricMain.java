c603eb1d58cd90bfd7fccb5d95b42ca564b91df6
jetchua123 <jet_chua@dlsu.edu.ph>
2020-02-19 18:46:32 +0800

    metrics added

---
+++ src/MetricMain.java
@@ -0,0 +1,20 @@
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MetricMain {
+    public static void main(String[] args) throws IOException {
+        Scanner user_input = new Scanner( System.in );
+        System.out.print("Enter Directory Name: ");
+        String pathDirectory = user_input.next( );
+        user_input.close();
+
+        HalsteadMetrics hal = Metricator.getMetrics(pathDirectory);
+        System.out.println("Vocabulary= "+ hal.getVocabulary());
+        System.out.println("Program Length= "+ hal.getProglen());
+        System.out.println("Volume= "+ hal.getVolume());
+        System.out.println("Effort= "+ hal.getEffort());
+        System.out.println("Number of delivered bugs= "+ hal.getTimeDelBugs());
+
+    }
+}
